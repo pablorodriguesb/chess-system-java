4677761fecf14c26a2d574427a9c8c49aa87f2a4
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-24 08:25:29 -0300

    Starting to implement Board and Piece

--- src/main/java/com/devpablo/application/Program.java
+++ src/main/java/com/devpablo/application/Program.java
@@ -1,12 +1,11 @@
 package com.devpablo.application;
 
-import com.devpablo.boardgame.Position;
+import com.devpablo.boardgame.Board;
 
 public class Program {
 
     public static void main(String[] args) {
         
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
     }
 }
