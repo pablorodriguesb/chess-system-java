c97bcefda0f348bcd6950d3df401c1134adfe7ed
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-24 09:55:01 -0300

    ChessException and ChessPosition

---
+++ src/main/java/com/devpablo/chess/ChessException.java
@@ -0,0 +1,8 @@
+package com.devpablo.chess;
+
+public class ChessException extends RuntimeException {
+    
+    public ChessException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/devpablo/chess/ChessMatch.java
+++ src/main/java/com/devpablo/chess/ChessMatch.java
@@ -1,33 +1,35 @@
 package com.devpablo.chess;
 
 import com.devpablo.boardgame.Board;
-import com.devpablo.boardgame.Position;
 import com.devpablo.chess.pieces.King;
 import com.devpablo.chess.pieces.Rook;
 
 public class ChessMatch {
-    
+
     private Board board;
-    
+
     public ChessMatch() {
         board = new Board(8, 8);
         initialSetup();
     }
-    
+
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i=0; i<board.getRows(); i++) {
-            for (int j=0; j<board.getColumns(); j++) {
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
                 mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
-    
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.BLACK), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.BLACK));
     }
 }
-
