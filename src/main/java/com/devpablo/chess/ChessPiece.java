6d4f62d220dc717e988836ad1bf344163fefd752
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-25 14:07:53 -0300

    Piece move count

--- src/main/java/com/devpablo/chess/ChessPiece.java
+++ src/main/java/com/devpablo/chess/ChessPiece.java
@@ -5,7 +5,9 @@ import com.devpablo.boardgame.Piece;
 import com.devpablo.boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
+
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color) {
         super(board);
@@ -15,14 +17,27 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
-    
+
+    public int getMoveCount() {
+        return moveCount;
+
+    }
+
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
-    
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
-    
+
 }
